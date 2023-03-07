5e1dd332c8c95583df6d7d6f85886981cd3e21a0
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-07 13:41:41 +0530

    feat: added isLoggedIn middleware

--- middleware/user.js
+++ middleware/user.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 exports.isNotLoggedIn = (req, res, next) => {
   let token;
@@ -36,3 +37,33 @@ exports.isNotLoggedIn = (req, res, next) => {
 
   next();
 };
+
+exports.isLoggedIn = async(req,res,next) => {
+  try {
+    let token;
+    if(req.cookies){
+      token = req.cookies
+    }
+
+    if(!token && req.header("Authorization")){
+      token = req.header("Authorization").replace("Bearer " , "")
+    }
+
+    if(!token){
+      return res.status(401).json({
+        message : "User is not logged in. Please log in to access this route"
+      })
+    }
+
+    const decoded = jwt.verify(token , process.env.JWT_SECRET)
+
+    const user = await User.findById(decoded.id)
+
+    req.user = user
+    next()
+  } catch(err) {
+    return res.status(500).json({
+      message : "Something went wrong in isLoggedIn middleware"
+    })
+  }
+}
